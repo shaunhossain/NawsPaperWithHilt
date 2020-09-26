4687a574be16d8f35c4065c032bf634d129a39c8
shaunhossain <shaunhossain95@gmail.com>
2020-09-27 00:47:52 +0600

    inaitial set up . just content dependencies and set up layout

---
+++ app/src/main/java/com/shaunhossain/nawspaperwithhilt/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.shaunhossain.nawspaperwithhilt
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.findNavController
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        val navController = this.findNavController(R.id.nav_host_fragment)
+
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = this.findNavController(R.id.nav_host_fragment)
+        return navController.navigateUp()
+    }
+}
